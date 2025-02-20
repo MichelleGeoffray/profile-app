b26de78a012d3058c2261a3a00817e310d2743a5
Mia <mgeoffra@purdue.edu>
2025-02-19 19:16:22 -0500

    Lab11

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
 import styles from "../styles/navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ mode, updateMode }) => {
-    return (
-        <nav className={`${styles["navbar"]}`}>
-            <ul>
-                <li>
-                    <a href="#" className="listItem1">Home</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Profiles</a>
-                </li>
-            </ul>
-            <button onClick={updateMode}>
-                {mode === "light" ? "Light Mode" : "Dark Mode"}
-            </button>
-        </nav>
-    );
+  return (
+    <nav className={`${styles["navbar"]}`}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+        <Link to="/about">About</Link>
+        </li>
+        <li>
+        <Link to="/add-profile">Add Profile</Link>
+        </li>
+      </ul>
+      <button onClick={updateMode}>
+        {mode === "light" ? "Light Mode" : "Dark Mode"}
+      </button>
+    </nav>
+  );
 };
+
 export default Navbar;
